c247619957b63c9a5b4aabafb2a553cd071984b8
raineer <delaritaraineer81@gmail.com>
2019-04-14 21:17:22 +0800

    create simple route

--- api/postContent.js
+++ api/postContent.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.json({
+        message: 'tae'
+    })
+});
+
 module.exports = router;
