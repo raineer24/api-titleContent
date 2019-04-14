6a7d378ce8a6e566efcac6a042cf990dcba94f29
raineer <delaritaraineer81@gmail.com>
2019-04-14 20:57:12 +0800

    res.json not res.render

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render({
+  res.json({
     message: err.message,
     error: req.app.get('env') === 'development' ? err : {}
   });
