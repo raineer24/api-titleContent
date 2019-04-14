e121fd91813b19dfa42c15708b1f449fcfc93091
raineer <delaritaraineer81@gmail.com>
2019-04-14 21:39:30 +0800

    api/v1/content

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api/v1/postContent', postContent);
+app.use('/api/v1/content', postContent);
 
 
 
