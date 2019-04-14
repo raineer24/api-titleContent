138d0bdeeb15db09391cc4fa4cdfa9d8609248ab
raineer <delaritaraineer81@gmail.com>
2019-04-14 22:55:33 +0800

    run every migration before the test

--- test/app.test.js
+++ test/app.test.js
@@ -1,3 +1,13 @@
-describe('POST Content', () => {
+const knex =require('../db/knex');
 
+describe('POST Content', () => {
+    before(() => {
+        //run migrations
+        Knex.migrate.latest()
+            .then(() => {
+                //run seeds
+                return knex.seed.run();
+            })
+        
+    });
 });
