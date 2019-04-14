0f223890a3d435a055a51dcac6fe3f6afe289eb2
raineer <delaritaraineer81@gmail.com>
2019-04-14 18:44:51 +0800

    contents.js

---
+++ contents.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        title: "The Anubis Gates",
+        content: "Brendan Doyle, a specialist in the work of the early-nineteenth century poet William Ashbless, reluctantly accepts an invitation from a millionaire to act as a guide to time-travelling tourists. But while attending a lecture given by Samuel Taylor Coleridge in 1810, he becomes marooned in Regency London, where dark and dangerous forces know about the gates in time"
+    },
+    {
+        title: "Till We Have Faces",
+        content: "In this timeless tale of two mortal princesses- one beautiful and one unattractive- C.S. Lewis reworks the classical myth of Cupid and Psyche into an enduring piece of contem"
+    },
+    {
+        title: "A Grief Observed",
+        content: "Written with love, humility, and faith, this brief but poignant volume was first published in 1961 and concerns the death of C. S. Lewis's wife"
+    },
+    {
+        title: "Holy Bible: King James Version",
+        content: "Great for all ages! All the majesty of the Authorized King James Version in a beautiful Black Leatherflex (Imitation Leather) Binding. "
+    },
+    {
+        title: "The Four Loves",
+        content: "The Four Loves summarizes four kinds of human love--affection, friendship, erotic love, and the love of God. Masterful without being magisterial, this book's wise"
+    },
+    {
+        title: "The Problem of Pain",
+        content: "For centuries people have been tormented by one question above all: If God is good and all-powerful, why does he allow his creatures to suffer pain?"
+    },
+    {
+        title: "The Screwtape Letters",
+        content: "A masterpiece of satire, this classic has entertained and enlightened readers the world over with its sly and ironic portrayal of human life from the vantage point of Screwta"
+    },
+    {
+        title: "Promise Yourself",
+        content: "Now a New York Times Bestseller! As a college student he spent 16 days in the Pacific Ocean with five guys and a crate of canned meat. "
+    }
+]
