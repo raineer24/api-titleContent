1e03e4403e468b94ec154cdf891d24e1d515e616
raineer <delaritaraineer81@gmail.com>
2019-04-14 20:43:21 +0800

    remove index user router

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
