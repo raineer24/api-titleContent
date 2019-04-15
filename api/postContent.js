b7e3e3c3f7d98d991da42c775b33f743b39b5d9b
raineer <delaritaraineer81@gmail.com>
2019-04-15 16:11:23 +0800

    eslint-disable-next-line no-multiple-spaces

--- api/postContent.js
+++ api/postContent.js
@@ -4,42 +4,46 @@ const router = express.Router();
 
 const queries = require('../db/queries');
 
+// eslint-disable-next-line consistent-return
 function isValidId(req, res, next) {
-    if (!isNaN(req.params.id)) return next();
-    next(new Error('Invalid ID'));
+  // eslint-disable-next-line no-restricted-globals
+  if (!isNaN(req.params.id)) return next();
+  next(new Error('Invalid ID'));
 }
 
 function validContent(content) {
-    const hasTitle = typeof content.title == 'string' && content.title.trim() != '';
-    const hasContent = typeof content.content == 'string' && content.content.trim() != '';
-    return hasTitle && hasContent;
+  // eslint-disable-next-line no-multi-spaces
+  const hasTitle =    typeof content.title === 'string' && content.title.trim() !== '';
+  // eslint-disable-next-line no-multi-spaces
+  const hasContent =    typeof content.content === 'string' && content.content.trim() !== '';
+  return hasTitle && hasContent;
 }
 
 router.get('/', (req, res) => {
-    queries.getAll().then(content => {
-        res.json(content);
-    });
- });
+  queries.getAll().then((content) => {
+    res.json(content);
+  });
+});
 
-router.get('/:id', isValidId, (req,res, next) => {
-    queries.getOne(req.params.id).then(content => {
-        if (content) {
-            res.json(content);
-        } else{
-            next();
-        }
-    });
+router.get('/:id', isValidId, (req, res, next) => {
+  queries.getOne(req.params.id).then((content) => {
+    if (content) {
+      res.json(content);
+    } else {
+      next();
+    }
+  });
 });
 
 router.post('/', (req, res, next) => {
-    if (validContent(req.body)) {
-        // insert into db
-        queries.create(req.body).then(contents => {
-            res.json(contents[0]);
-        });
-    } else {
-        next(new Error('Invalid Content'));
-    }
+  if (validContent(req.body)) {
+    // insert into db
+    queries.create(req.body).then((contents) => {
+      res.json(contents[0]);
+    });
+  } else {
+    next(new Error('Invalid Content'));
+  }
 });
 
 module.exports = router;
