3b670d00b3c8880fcfd028f15c23ef724074315e
raineer <delaritaraineer81@gmail.com>
2019-04-15 10:23:43 +0800

    middleware function isValidId

--- api/postContent.js
+++ api/postContent.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 const queries = require('../db/queries');
 
+function isValidId(req, res, next) {
+    
+}
+
 router.get('/', (req, res) => {
     queries.getAll().then(contents => {
         res.json(contents)
