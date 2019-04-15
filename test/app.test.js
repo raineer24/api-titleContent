7aa3c845e7992b863385782413f617f233aa3e9f
raineer <delaritaraineer81@gmail.com>
2019-04-15 12:42:23 +0800

    add test for showing one record content by id, mocha

--- test/app.test.js
+++ test/app.test.js
@@ -30,4 +30,34 @@ describe('POST Content', () => {
             });
 
     });
+
+    it('Show one record by id', (done) => {
+        request(app)
+            .get('/api/v1/content/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).to.be.a('object');
+                expect(response.body).to.deep.equal(fixtures.contents[0]);
+                done();
+            });
+
+    });
+
+    it('Show one record by id', (done) => {
+        request(app)
+            .get('/api/v1/content/5')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).to.be.a('object');
+                expect(response.body).to.deep.equal(fixtures.contents[4]);
+                done();
+            });
+
+    });
+
+    
 });
