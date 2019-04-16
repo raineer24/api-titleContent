e665277cd9c11b02f827b86246a75a8e1600e650
raineer <delaritaraineer81@gmail.com>
2019-04-16 17:33:46 +0800

    validate id from isvalidId middleware function, create router query delete id

--- api/postContent.js
+++ api/postContent.js
@@ -57,6 +57,12 @@ router.put('/:id', isValidId, (req, res, next) => {
   }
 });
 
-router.delete('/:id', isValidId, (req, res) => {});
+router.delete('/:id', isValidId, (req, res) => {
+  queries.delete(req.params.id).then(() => {
+    res.json({
+      deleted: true,
+    });
+  });
+});
 
 module.exports = router;
