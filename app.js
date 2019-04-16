eee0de4eca77fe8c60c2efaeb9dd0cfb0bafb94d
raineer <delaritaraineer81@gmail.com>
2019-04-16 23:49:47 +0800

    corsII, wrong commits

--- app.js
+++ app.js
@@ -13,24 +13,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type,Accept, Authorization',
+  );
+  if (req.method === 'OPTIONS') {
+    res.header(
+      'Access-Control-Allow-Methods',
+      'PUT, POST, ,PATCH, DELETE, GET',
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use('/api/v1/content', postContent);
 
-
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((_req, _res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  
-
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, _next) => {
   // render the error page
   res.status(err.status || 500);
   res.json({
     message: err.message,
-    error: req.app.get('env') === 'development' ? err : {}
+    error: req.app.get('env') === 'development' ? err : {},
   });
 });
 
