dab5ef81ac0fcc16eabfdf996c9e52ace35529da
raineer <delaritaraineer81@gmail.com>
2019-05-13 21:35:17 +0800

    multer

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
 const logger = require('morgan');
 
 const app = express();
@@ -9,9 +10,20 @@ const app = express();
 const postContent = require('./api/postContent');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 1000000
+  })
+);
+
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -29,7 +41,10 @@ app.use((req, res, next) => {
   next();
 });
 
+
+
 app.use('/api/v1/content', postContent);
+
 
 // catch 404 and forward to error handler
 app.use((_req, _res, next) => {
